f3eed9c7327c450b38f776088a07851d3004c301
Yoyiqw <amirhan2005men@gmail.com>
2022-07-20 18:01:18 +0600

    Swiper update

--- js/slider/index.js
+++ js/slider/index.js
@@ -1,11 +1,11 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.experiment__swiper', {
+    slidesPerView: 3,
     loop: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
   
-    
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
